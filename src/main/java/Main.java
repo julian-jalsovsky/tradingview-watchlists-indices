f9dff1f1c726f0e3dbac5c02685d66ca9003ba72
Julian Jalsovsky <julian.jalsovsky@gmail.com>
2023-09-20 18:20:16 +0200

    Add functionality for writing watchlists into textfiles

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,31 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
 
-        Watchlist watchlistSP500 = new SP500Watchlist();
-        System.out.println(watchlistSP500.getWatchlist());
+        HashMap<String, Watchlist> watchlists = new HashMap<>();
+        watchlists.put("S&P500", new SP500Watchlist());
+        watchlists.put("Nasdaq100", new NasdaqWatchlist());
+        watchlists.put("Dow30", new DowWatchlist());
 
-        Watchlist watchlistNasdaq = new NasdaqWatchlist();
-        System.out.println(watchlistNasdaq.getWatchlist());
+        writeToFile(watchlists);
+    }
 
-        Watchlist watchlistDow = new DowWatchlist();
-        System.out.println(watchlistDow.getWatchlist());
+    private static void writeToFile(HashMap<String, Watchlist> watchlists) {
+        for (Map.Entry<String, Watchlist> watchlist : watchlists.entrySet()) {
+            BufferedWriter writer;
+            String fileName = "watchlist-" + watchlist.getKey() + ".txt";
+            try {
+                writer = new BufferedWriter(new FileWriter(fileName));
+                writer.write(watchlist.getValue().getWatchlist());
+                writer.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
