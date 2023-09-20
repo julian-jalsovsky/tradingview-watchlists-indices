8cdbf1d319c9e66385bf471237122621be07355c
Julian Jalsovsky <julian.jalsovsky@gmail.com>
2023-09-20 16:25:14 +0200

    Initial Commit

--- src/main/java/SP500Watchlist.java
+++ src/main/java/SP500Watchlist.java
@@ -12,44 +12,13 @@ import java.util.*;
 
 public class SP500Watchlist extends Watchlist {
 
-    private final String url;
-    private final Map<String, String> symbols;
-
     public SP500Watchlist() throws IOException, CsvException {
-        url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies";
-        symbols = new TreeMap<>();
-        extractSymbols();
-        addExchanges();
-    }
-
-    private void addExchanges() throws IOException, CsvException {
-        CSVReader reader = new CSVReaderBuilder(new FileReader("listed_companies.csv")).build();
-        List<String[]> myEntries = reader.readAll();
-
-        Map<String, String> map = new HashMap<>();
-        for (String[] entry : myEntries) {
-            map.put(entry[0], entry[1]);
-        }
-
-        for (String symbol : symbols.keySet()) {
-            symbols.replace(symbol, map.get(symbol));
-        }
-    }
-
-    @Override
-    public String getWatchlist() {
-        StringBuilder watchlist = new StringBuilder();
-        for (Map.Entry<String, String> set : symbols.entrySet()) {
-            watchlist.append(set.getValue())
-                    .append(":")
-                    .append(set.getKey())
-                    .append(",");
-        }
-        return watchlist.toString();
+        super();
     }
 
     @Override
     protected void extractSymbols() {
+        String url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies";
         Document html = requestHTML(url);
         Element table = html.getElementById("constituents");
 
--- src/main/java/Watchlist.java
+++ src/main/java/Watchlist.java
@@ -1,12 +1,52 @@
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public abstract class Watchlist {
 
-    public abstract String getWatchlist();
+    protected final Map<String, String> symbols;
+
+    public Watchlist() throws IOException, CsvException {
+        symbols = new TreeMap<>();
+        extractSymbols();
+        addExchanges();
+    }
+
     protected abstract void extractSymbols();
+
+    private void addExchanges() throws IOException, CsvException {
+        CSVReader reader = new CSVReaderBuilder(new FileReader("listed_companies.csv")).build();
+        List<String[]> myEntries = reader.readAll();
+
+        Map<String, String> map = new HashMap<>();
+        for (String[] entry : myEntries) {
+            map.put(entry[0], entry[1]);
+        }
+
+        for (String symbol : symbols.keySet()) {
+            symbols.replace(symbol, map.get(symbol));
+        }
+    }
+
+    public String getWatchlist() {
+        StringBuilder watchlist = new StringBuilder();
+        for (Map.Entry<String, String> set : symbols.entrySet()) {
+            watchlist.append(set.getValue())
+                    .append(":")
+                    .append(set.getKey())
+                    .append(",");
+        }
+        return watchlist.toString();
+    }
 
     protected Document requestHTML(String url) {
         Document html;
